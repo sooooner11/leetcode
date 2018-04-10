bc24a5df0baebdb33baf262fef7a9c73113313b5
sooooner11 <34790792+sooooner11@users.noreply.github.com>
2018-04-10 22:14:22 +0800

    191 Number of 1 Bits

---
+++ Bit Manipulation/Solution191.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Solution191 {
+    public int hammingWeight(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++) {
+        	ans+=n&1;
+        	n>>>=1;
+        }
+        return ans;
+    }
+}
